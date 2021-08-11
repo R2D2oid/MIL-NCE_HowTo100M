66bf565f34a286e2076e17d6898041d55f5b4f15
Zahra Vaseqi <zahrav@cedar1.cedar.computecanada.ca>
2021-08-10 19:49:55 -0700

    sbatch job: cuda memory issue stands

---
+++ run_eval_msrvtt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=def-mudl
+#SBATCH --gres=gpu:p100:1 
+#SBATCH --cpus-per-task=6         # CPU cores/threads
+#SBATCH --mem=100G                # memory (per node)
+#SBATCH --time=0-02:00            # time (DD-HH:MM)
+
+module load python/3.6
+source env_h100m/bin/activate
+
+python eval_msrvtt.py --batch_size=16  --num_thread_reader=20 --num_windows_test=10 --eval_video_root=/home/zahrav/datasets/MSRVTT/TestVideo --pretrain_cnn_path=/home/zahrav/scratch/repos/models/s3d_howto100m.pth
